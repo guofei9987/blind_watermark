df03430daff09e780e7bae36768e147b37a99f79
guofei9987 <guofei9987@foxmail.com>
2021-03-06 21:55:32 +0800

    embedding watermark on exif is not involved in this project

--- examples/命令行.py
+++ examples/命令行.py
@@ -40,75 +40,6 @@ def get_str(str_get, str_info):
     return str_get
 
 
-def mod_00():
-    attempts = 0
-    success = False
-    while attempts < 3000 and not success:
-        try:
-            yuantu_name_head = "abc"
-            yuantu_name_head = get_str(yuantu_name_head, "输入你的原图文件名,要带后缀,例如：abc.png/abc.jpg|不支持带汉字文件名")
-            source = './yuantu/' + yuantu_name_head
-            target = './shuchu/233313213.jpg'
-
-            shu_liang = 50
-            shu_liang = get_value(shu_liang, "输入你想生成的图片数量，数量无限制！")
-            i = 1
-            int(i)
-            int(shu_liang)
-            while i < shu_liang + 1:
-                ge_shi = str('.png')
-
-                source_in_loop = source
-                target_in_loop = './shuchu/out_' + str(i) + "_" + yuantu_name_head
-
-                print(target)
-                print(source)
-                shutil.copy(source, target_in_loop)
-                print("复制完成")
-                cmd = ".\exiftool  -artist=" + str(i) + " " + target_in_loop
-                print(cmd)
-                r1 = os.popen(cmd)
-                print(r1.read())
-                print(str(i / shu_liang * 100) + "%已经完成")
-                i = i + 1
-
-            input("复制完成")
-
-
-
-        except:
-            print("程序出错，可能是你输入的文件名称错误,,请从头开始")
-            attempts += 1
-            if attempts == 3000:
-                break
-    return 0
-
-
-def mod_01():
-    attempts = 0
-    success = False
-    while attempts < 3000 and not success:
-        try:
-            print("EXFI_图片水印解密模式：")
-            print("请将你需要解密文件放置于jiemi文件夹下")
-            jiemi_name = "abc"
-            jiemi_name = get_str(jiemi_name, "输入你想要解密的文件名,要带后缀,例如：abc.png/abc.jpg|不支持带汉字文件名")
-            jiemi_name = 'jiemi/' + jiemi_name
-            print("正在寻找" + jiemi_name + "待解密文件")
-            cmd = ".\exiftool  -artist" + " " + jiemi_name
-            # print(cmd)
-            r1 = os.popen(cmd)
-
-            print("解密信息如下，如为空，则此图无水印，数字为其序号")
-            print(r1.read())
-        except:
-            print("程序出错，可能是你输入文件名称错误,,请从头开始")
-            attempts += 1
-            if attempts == 3000:
-                break
-    return 0
-
-
 def mod_1():
     attempts = 0
     success = False
@@ -240,48 +171,22 @@ print("程序开始运行：")
 print("如程序出错，请检查你的输入内容，重新打开此程序即可")
 print("请注意，本程序需要和其下文件夹配合工作他们分别是:\n yuantu:放置原图\n shuiyin；存放水印\n jiemi存放等待解密文件\n shuichu输出文件夹\n 复制此程序时，请直接打包本程序所在文件夹")
 
-# 选择工作核心
-print("请选择程序工作内核：\n 1: Exif水印模式（不损失画质仅可查原画） \n 2: 盲水印模式（增加噪点，截图可查）")
-core_mode = 0
-core_mode = get_value(core_mode, "请选输入工作内核序号")
-if core_mode != 0 and core_mode != 1 and core_mode != 2:
-    print("你输入的核心序号不存在")
+print("请选择程序工作模式：\n 1: 默认工作模式（自动添加一定数量水印）\n 2: 自定义模式（请在文件夹添加你的自定义水印）\n 3: 水印解密模式（需要输入密码）")
+work_mode = 0
+work_mode = get_value(work_mode, "请选输入工作模式序号")
+if work_mode != 0 and work_mode != 1 and work_mode != 2 and work_mode != 3:
+    print("你输入的工作模式序号不存在")
+if work_mode == 0:
+    print("错误次数过多，已关闭")
+if work_mode == 1:
+    res = 1
+    res = mod_1()
 
-if core_mode == 0:
-    print("核心选择，错误次数过多，已关闭")
-if core_mode == 1:
-    print("已选择EXIF水印模式")
-    print("请选择程序工作模式：\n 1: 默认工作模式\n \n 2: 水印解密模式")
-    work_mode = 0
-    work_mode = get_value(work_mode, "请选输入工作模式序号")
-    if work_mode != 1 and work_mode != 1 and work_mode != 2:
-        print("你输入的工作模式序号不存在")
-    if work_mode == 1:
-        res = 1
-        res = mod_00()
-    if work_mode == 2:
-        res = 1
-        res = mod_01()
-
-if core_mode == 2:
-    # 选择工作模式
-    print("已选择盲水印模式")
-    print("请选择程序工作模式：\n 1: 默认工作模式（自动添加一定数量水印）\n 2: 自定义模式（请在文件夹添加你的自定义水印）\n 3: 水印解密模式（需要输入密码）")
-    work_mode = 0
-    work_mode = get_value(work_mode, "请选输入工作模式序号")
-    if work_mode != 0 and work_mode != 1 and work_mode != 2 and work_mode != 3:
-        print("你输入的工作模式序号不存在")
-    if work_mode == 0:
-        print("错误次数过多，已关闭")
-    if work_mode == 1:
-        res = 1
-        res = mod_1()
-
-    if work_mode == 2:
-        res = 1
-        res = mod_2()
-    if work_mode == 3:
-        res = 1
-        res = mod_3()
+if work_mode == 2:
+    res = 1
+    res = mod_2()
+if work_mode == 3:
+    res = 1
+    res = mod_3()
 
 input("程序运行完毕，需要再次使用请重新打开")
